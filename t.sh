4bb73985b4fbb5d9930c4ddf8b9494894addbce2
zhusongqi <815723901@qq.com>
2017-02-21 17:42:11 +0800

    weew

--- t.sh
+++ t.sh
@@ -15,7 +15,7 @@ function gitCommit() {
     res=$(equals 'rejected' "${message}") 
     if [ "${res}" == "Y" ] 
     then 
-        $(gitConflict)
+        echo $(gitConflict)
     else 
         echo '[OK] sublimt git' 
     fi
