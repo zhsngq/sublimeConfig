7b0f1e7365c72f8d8ea12527864e0d6d87d16fb8
zhusongqi <815723901@qq.com>
2017-02-21 11:58:27 +0800

    "$commitMessage"

--- gr.sh
+++ gr.sh
@@ -3,7 +3,7 @@ function gitCommit() {
     echo 'set message ..'
     read commitMessage &&
     git add . &&
-    git commit -m '$commitMessage' &&
+    git commit -m '"$commitMessage"' &&
     git push && 
     echo '[OK] sublimt git'
 }
