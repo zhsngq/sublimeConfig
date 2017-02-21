0920bd561bc61ffbb2995d18f7ac11f73dda2b91
zhusongqi <815723901@qq.com>
2017-02-21 14:44:02 +0800

    12

--- gr.sh
+++ gr.sh
@@ -1,22 +1,4 @@
 #!/bin/bash
-function gitCommit() {
-    echo 'set message ..'
-    read commitMessage &&
-    git add . &&
-    git commit -m $commitMessage &&
-    git push && 
-    echo '[OK] sublimt git'
-}
-echo 'update...' 
-message=(`git pull`)
-if [[ ${message[0]}=="Already"]] 
-then 
-    echo 'no update'
-    # gitCommit
-else 
-    echo 'update'
-    # git stash &&
-    # git pull &&
-    # git stash pop
-    # gitCommit
-fi
+source t.sh
+
+gitCommit
--- t.sh
+++ t.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
-message=`git status`
-echo $message;
+function equals (){
+    if [[ ${2/${1}//} == $2 ]]
+    then
+        echo 'N'
+    else
+        echo 'Y'
+    fi
+}
+
+function gitCommit() {
+    echo 'set message ..'
+    read commitMessage
+    git add . &&
+    git commit -m $commitMessage &&
+    git push && 
+    echo '[OK] sublimt git'
+}
+
+function gitConflict(){
+    git stash &&
+    git pull &&
+    git stash pop
+    echo '[ERROR] git Conflict'
+}
