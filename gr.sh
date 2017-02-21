fbad11d3659bfbe5627220a4663e8649cbdd571e
zhusongqi <815723901@qq.com>
2017-02-21 10:51:39 +0800

    $commitMessage

--- gr.sh
+++ gr.sh
@@ -2,19 +2,16 @@
 function gitCommit() {
     git add . &&
     echo 'set message ..'
-    read message &&
-    git commit -m '$message' &&
+    read commitMessage &&
+    git commit -m '$commitMessage' &&
     git push && 
     echo '[OK] sublimt git'
 }
 echo 'update...' 
 message=(`git pull`)
 if [ "$message[0]"x = "Updating"x ]; then 
-    echo 'stash'
     git stash && 
-    echo 'pull'
     git pull &&
-    echo 'gitCommit'
     gitCommit  
     git stash pop 
 else 
