564cb4c2296e5067ed4b0876667c7cdd9a680f33
zhusongqi <815723901@qq.com>
2017-02-21 15:34:20 +0800

    12

--- gr.sh
+++ gr.sh
@@ -4,7 +4,8 @@ source t.sh
 message=$(git status)
 echo ${message}
 res=$(equals "nothing to commit" ${message})
-if test $res=='N' 
+echo ${res}
+if test ${res}=='N' 
     then 
         gitCommit
     else 
