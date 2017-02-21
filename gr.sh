ee1f3a68ebe9fc2d6bff6a4852cc5c5cfd081cda
zhusongqi <815723901@qq.com>
2017-02-21 10:13:03 +0800

    12

--- gr.sh
+++ gr.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+function gitCommit() {
+    git add . &&
+    read message &&
+    git commit -m '$message' &&
+    git pull
+}
 echo 'update...' 
 message=(`git pull`)
 if [ "$message[0]"x = "Updating"x ]; then 
-    echo 'ct'
+    gitCommit
 else
 # 冲突
+    git stash && 
+    git pull &&
+    git stash pop
 fi
